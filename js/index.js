d4b8f2db895e02523cf2f8381e9d0f3d8502aa89
ConstantinNoettgen <constantin-noettgen@gmx.de>
2021-12-30 11:10:01 +0100

    Initial Commit

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 
 var ergebnis
+
 function meterinKilometer() {
-    let anzahlMeter = parseInt(document.getElementById("meter").value);
+    let  anzahlMeter = parseInt(document.getElementById("meter").value);
     console.log(anzahlMeter);
     ergebnis = anzahlMeter / 1000;
     console.log(ergebnis);
-    document.getElementById("Ausgabe1").innerHTML = ergebnis;
-
+    document.getElementById("TabelleKilometer").innerHTML = ergebnis;
     return ergebnis
 }
 
@@ -14,7 +14,7 @@ function kilometerInMeilen() {
     let anzahlKilometer = ergebnis
     let ergebnis2 = anzahlKilometer / parseFloat(1.609) ;
     console.log("anzK ist" + anzahlKilometer)
-    document.getElementById("Ausgabe2").innerHTML = ergebnis2;
+    document.getElementById("TabelleMeilen").innerHTML = ergebnis2;
     console.log("erg2 ist" + ergebnis2);
     return ergebnis2;
 }
@@ -23,7 +23,7 @@ function kilometerInYards() {
     let anzahlKilometer = ergebnis
     let ergebnis3 = anzahlKilometer * 1094;
     console.log("anzK ist" + anzahlKilometer)
-    document.getElementById("Ausgabe3").innerHTML = ergebnis3;
+    document.getElementById("TabelleYards").innerHTML = ergebnis3;
     console.log("erg2 ist" + ergebnis3);
     return ergebnis3;
 }
@@ -31,13 +31,32 @@ function kilometerInFuß() {
     let anzahlKilometer = ergebnis
     let ergebnis4 = anzahlKilometer * 3281;
     console.log("anzK ist" + anzahlKilometer)
-    document.getElementById("Ausgabe4").innerHTML = ergebnis4;
+    document.getElementById("TabelleFuß").innerHTML = ergebnis4;
     console.log("erg2 ist" + ergebnis4);
     return ergebnis4;}
 function gesamteRechnung() {
+    Überprüfe();
     meterinKilometer();
     kilometerInMeilen();
     kilometerInYards();
     kilometerInFuß()
+}
+function Überprüfe() {
+    let  anzahlMeter = parseInt(document.getElementById("meter").value);
+    if (isNaN(anzahlMeter)) {
+        alert("Überprüfe deine Eingabe, du hast keine Zahl eingegeben.");
+        return anzahlMeter
+
+    }
+}
+
+function hide_elements()
+{
+    var elementNames = hide_elements.arguments;
+    for (var i=0; i<elementNames.length; i++)
+    {
+        var elementName = elementNames[i];
+        document.getElementById(elementName).style.display='none';
+    }
 }
 
